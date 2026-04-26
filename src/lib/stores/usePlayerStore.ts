b220e4152f70b2ff8b0f9896e4747cf6d809305f
Tomer Kamar <tomer.kamar@gmail.com>
2026-04-26 21:48:53 +0300

    feat: Zustand stores for player, sync, and song state

---
+++ src/lib/stores/usePlayerStore.ts
@@ -0,0 +1,26 @@
+import { create } from 'zustand'
+import { MediaBridge, PlayerState } from '../adapters/MediaBridge'
+
+interface PlayerSlice {
+  bridge: MediaBridge | null
+  state: PlayerState
+  duration_ms: number
+  setBridge: (bridge: MediaBridge) => void
+  setState: (state: PlayerState) => void
+  setDuration: (ms: number) => void
+  play: () => void
+  pause: () => void
+  seek: (ms: number) => void
+}
+
+export const usePlayerStore = create<PlayerSlice>((set, get) => ({
+  bridge: null,
+  state: 'idle',
+  duration_ms: 0,
+  setBridge: (bridge) => set({ bridge }),
+  setState: (state) => set({ state }),
+  setDuration: (duration_ms) => set({ duration_ms }),
+  play: () => get().bridge?.play(),
+  pause: () => get().bridge?.pause(),
+  seek: (ms) => get().bridge?.seek(ms),
+}))
